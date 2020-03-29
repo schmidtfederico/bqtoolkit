a1c866dabe37382276931d90128a376e0e6f2593
Federico Schmidt <schmidt.federico@hotmail.com>
2020-03-29 17:35:26 -0300

    Use Unittest assertion functions. Add more tests.

--- tests/test_bqtable.py
+++ tests/test_bqtable.py
@@ -17,26 +17,53 @@ class BQLibTest(unittest.TestCase):
 
     def test_init_from_path(self):
         # Semicolon can be used as project delimiter
-        assert BQTable.from_table_path('project.dataset.table') == BQTable.from_table_path('project:dataset.table')
+        self.assertEqual(BQTable.from_table_path('project.dataset.table'),
+                         BQTable.from_table_path('project:dataset.table'))
 
-        assert BQTable.from_table_path('project.dataset.table_name') == BQTable('project', 'dataset', 'table_name')
+        self.assertEqual(BQTable.from_table_path('project.dataset.table_name'),
+                         BQTable('project', 'dataset', 'table_name'))
 
-        assert BQTable.from_table_path('p.d.table$partiton') == BQTable('p', 'd', 'table')
+        self.assertEqual(BQTable.from_table_path('p.d.table$partiton'),
+                         BQTable('p', 'd', 'table'))
 
         # Hyphens should be allowed in project id only.
-        assert BQTable.from_table_path('project-id.dataset_id.t') == BQTable('project-id', 'dataset_id', 't')
+        self.assertEqual(BQTable.from_table_path('project-id.dataset_id.t'),
+                         BQTable('project-id', 'dataset_id', 't'))
 
         with pytest.raises(ValueError):
             BQTable.from_table_path('project-id.dataset-id.t')
+
+    @pytest.mark.skipif('GOOGLE_APPLICATION_CREDENTIALS' not in os.environ, reason='Undefined Google credentials')
+    def test_client_gets_created_if_missing(self):
+        t = BQTable.from_table_path('bqtoolkit.tmp.table')
+
+        self.assertIsNotNone(t.bq_client, 'bq_client should be populated when not defined in constructor')
 
     @pytest.mark.skipif(sys.version_info < (3, 5), reason='Required unittest.mock')
     def test_set_bqtable_client(self):
         my_client = mock.MagicMock()
 
-        assert BQTable('p', 'd', 't', client=my_client).bq_client == my_client
+        self.assertEqual(BQTable('p', 'd', 't', client=my_client).bq_client, my_client)
 
         # A specific client can be passed to from_table_path method too.
-        assert BQTable.from_table_path('p.d.t', client=my_client).bq_client == my_client
+        self.assertEqual(BQTable.from_table_path('p.d.t', client=my_client).bq_client, my_client)
+
+    def test_client_is_not_part_of_object_state(self):
+        t = BQTable.from_table_path('bqtoolkit.tmp.table')
+
+        object_state = t.__getstate__()
+
+        self.assertFalse('_bq_client' in object_state)
+
+    @pytest.mark.skipif(sys.version_info < (3, 5), reason='Required unittest.mock')
+    @pytest.mark.skipif('GOOGLE_APPLICATION_CREDENTIALS' not in os.environ, reason='Undefined Google credentials')
+    def test_client_is_not_used_for_object_comparison(self):
+        my_client = mock.MagicMock()
+
+        table_path = 'bqtoolkit.tmp.table'
+
+        if BQTable.from_table_path(table_path) != BQTable.from_table_path(table_path, client=my_client):
+            self.fail('Changing the BQTable\'s client changed the output of equality comparison')
 
     @pytest.mark.skipif('GOOGLE_APPLICATION_CREDENTIALS' not in os.environ, reason='Undefined Google credentials')
     def test_is_serializable(self):
