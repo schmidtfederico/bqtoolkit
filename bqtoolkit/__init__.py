8e2ce11ef36b1ad19d9678489eb4cca709b51e20
Federico Schmidt <schmidt.federico@hotmail.com>
2022-10-04 15:25:09 -0300

    Bump version

--- bqtoolkit/__init__.py
+++ bqtoolkit/__init__.py
@@ -1,4 +1,4 @@
 __project__ = "bqtoolkit"
 __author__ = "Federico Schmidt"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __license__ = "MIT"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 
 with open('README.md', 'r') as f:
     long_description = f.read()
